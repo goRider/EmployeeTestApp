6207f79b74b50f4c268c500ab4de829284ece8af
goRider <devgwo18@gmail.com>
2019-07-15 17:17:44 -0500

    Revert "Revert "Add project files.""
    This reverts commit 82c1adb77842479b4eb6b16eb0a7f184147b828e.

---
+++ EmployeeTracker/Data/EmployeeContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EmployeeTracker.Models;
+
+namespace EmployeeTracker.Data
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext() : base("name=con")
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
