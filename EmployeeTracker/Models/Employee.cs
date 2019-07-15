352c49c4022a3b96c2e1bbc5845fdfedea1d2f53
goRider <devgwo18@gmail.com>
2019-07-15 17:02:59 -0500

    Add project files.

---
+++ EmployeeTracker/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeTracker.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeID { get; set; }
+        public string EmployeeName { get; set; }
+        public string JobTitle { get; set; }
+    }
+}
